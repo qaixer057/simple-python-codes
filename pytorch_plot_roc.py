f458d5c490ebab10bc5342843331ea206b0aa8af
Qaiser Abbas <mqaiser617@gmail.com>
2020-06-10 23:29:01 +0500

    Create pytorch_plot_roc.py
    A code snippet to plot ROC curve using matplotlib and pytorch trained model

---
+++ pytorch_plot_roc.py
@@ -0,0 +1,16 @@
+#import all requried packages, 
+#from roc_curve get fpr and tpr and use matplotlib to plot the ROC
+fpr, tpr, _ = roc_curve(y_true, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC')
+plt.legend(loc="lower right")
+plt.show()
