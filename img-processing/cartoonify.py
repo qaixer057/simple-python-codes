2d24fa6d2c66acec69745c981421e1208ae88072
Qaiser Abbas <mqaiser617@gmail.com>
2023-02-12 23:46:37 +0500

    deleted image view for better results

--- img-processing/cartoonify.py
+++ img-processing/cartoonify.py
@@ -17,9 +17,6 @@ edges = cv2.adaptiveThreshold(
 color = cv2.bilateralFilter(img, 9, 250, 250)
 cartoon = cv2.bitwise_and(color, color, mask=edges)
 
-
-cv2.imshow("Image", img)
-cv2.imshow("edges", edges)
 cv2.imshow("Cartoon", cartoon)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
