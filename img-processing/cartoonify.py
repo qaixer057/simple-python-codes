a4d7298619fc0d4d151cbcdb0ce78b81887b483c
Qaiser Abbas <mqaiser617@gmail.com>
2023-02-12 23:47:50 +0500

    checking github desktop

--- img-processing/cartoonify.py
+++ img-processing/cartoonify.py
@@ -1,12 +1,9 @@
-# importing libraries
 # taken from geeksforgeeks
 import cv2
 import numpy as np
 
-# reading image
 img = cv2.imread("qaiser.jpg")
 
-# Edges
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)
 edges = cv2.adaptiveThreshold(
