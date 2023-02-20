a84cbac5221ea5c1491e5d042752b71ed224ea3a
Qaiser Abbas <mqaiser617@gmail.com>
2023-02-20 20:54:02 +0500

    Update isic_data_sorting.py

--- isic_data_sorting.py
+++ isic_data_sorting.py
@@ -1,3 +1,4 @@
+# can be used to sort other csv datasets
 import os
 import shutil
 import random
@@ -16,36 +17,51 @@ targetVASC = "D:\\NewData\\VASC"
 targetSCC = "D:\\NewData\\SCC"
 
 
-
 train_samples = 0
 
 for line in open("D:\\ISIC_2019_Training_GroundTruth.csv").readlines()[1:]:
-	split_line = line.split(",")
-	img_file = split_line[0]
-	melanoma = split_line[1]
-	NV = split_line[2]
-	BCC = split_line[3]
-	AK = split_line[4]
-	BKL = split_line[5]
-	DF = split_line[6]
-	VASC = split_line[7]
-	SCC = split_line[8]
+    split_line = line.split(",")
+    img_file = split_line[0]
+    melanoma = split_line[1]
+    NV = split_line[2]
+    BCC = split_line[3]
+    AK = split_line[4]
+    BKL = split_line[5]
+    DF = split_line[6]
+    VASC = split_line[7]
+    SCC = split_line[8]
 
-	if int(float(melanoma)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetMel+"\\"+img_file+".jpg")
-	elif int(float(NV)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetNV+"\\"+img_file+".jpg")
-	elif int(float(BCC)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetBCC+"\\"+img_file+".jpg")
-	elif int(float(AK)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetAK+"\\"+img_file+".jpg")
-	elif int(float(BKL)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetBKL+"\\"+img_file+".jpg")
-	elif int(float(DF)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetDF+"\\"+img_file+".jpg")
-	elif int(float(VASC)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetVASC+"\\"+img_file+".jpg")
-	elif int(float(SCC)) == 1:
-			shutil.move( directory+"\\"+img_file+".jpg", targetSCC+"\\"+img_file+".jpg")
+    if int(float(melanoma)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetMel + "\\" + img_file + ".jpg"
+        )
+    elif int(float(NV)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetNV + "\\" + img_file + ".jpg"
+        )
+    elif int(float(BCC)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetBCC + "\\" + img_file + ".jpg"
+        )
+    elif int(float(AK)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetAK + "\\" + img_file + ".jpg"
+        )
+    elif int(float(BKL)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetBKL + "\\" + img_file + ".jpg"
+        )
+    elif int(float(DF)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetDF + "\\" + img_file + ".jpg"
+        )
+    elif int(float(VASC)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetVASC + "\\" + img_file + ".jpg"
+        )
+    elif int(float(SCC)) == 1:
+        shutil.move(
+            directory + "\\" + img_file + ".jpg", targetSCC + "\\" + img_file + ".jpg"
+        )
 
-print('Completed')
+print("Completed")
