c0d405d81419f0135a4d4600f49c40fc5966d5dc
Qaiser Abbas <mqaiser617@gmail.com>
2023-02-24 17:33:36 +0500

    adding wav2vec file , working fine
    using librosa, transofrmers, wav2vec for video transcription

--- video_subtitles/wav2vec_test.py
+++ video_subtitles/wav2vec_test.py
@@ -1,7 +1,45 @@
 import torch
+import moviepy.editor as mp
 import librosa
 import numpy as np
 import soundfile as sf
 from scipy.io import wavfile
-from IPython.display import Audio
+import pysrt
+
+# from IPython.display import Audio
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+
+video = mp.VideoFileClip("video.mp4")
+audio = video.audio.write_audiofile("temp_audio.wav")
+
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+temp_file = "temp_audio.wav"
+
+data = wavfile.read(temp_file)
+framerate = data[0]
+sounddata = data[1]
+time = np.arange(0, len(sounddata)) / framerate
+print("Sampling rate:", framerate, "Hz")
+
+input_audio, _ = librosa.load(temp_file, sr=16000)
+
+input_values = tokenizer(input_audio, return_tensors="pt").input_values
+logits = model(input_values).logits
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = tokenizer.batch_decode(predicted_ids)[0]
+del temp_file
+print(transcription)
+
+print("writing to external srt file...")
+srt_file = pysrt.SubRipFile()
+srt_file.append(
+    pysrt.SubRipItem(
+        index=1,
+        text=transcription,
+        start=pysrt.SubRipTime(0, 0, 0),
+        end=pysrt.SubRipTime(hours=0, minutes=0, seconds=59),
+    )
+)
+srt_file.save("video.srt", encoding="utf-8")
