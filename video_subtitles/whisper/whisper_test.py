bfe95abc217c34f6ef5835a80b01c9117922ff6b
Qaiser Abbas <mqaiser617@gmail.com>
2023-02-27 17:33:37 +0500

    update whisper_test.py

--- video_subtitles/whisper/whisper_test.py
+++ video_subtitles/whisper/whisper_test.py
@@ -1,15 +1,15 @@
 import whisper
-
+import json
 model = whisper.load_model("base")
 result = model.transcribe("trump.mp3")
 encoded_text = result["text"].encode("utf-8")
-print(result["text"])
+# print(result["text"])
 
-print("===============what is segments=================")
-print(result["segments"])
+# print("===============what is segments=================")
+# print(result["segments"])
 
-
+print(result)
 # print(encoded_text)
 # print(type(result))
-# with open("result.txt", 'wb') as f:
-	# f.write(encoded_text)
+with open("result.txt", 'w') as f:
+	f.write(json.dumps(result))
